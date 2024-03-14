8dc26fd7a12f59e1b0d737e477090c4a5be0102b
Egor <tropin.egor@gmail.com>
2024-03-14 10:51:50 +0800

    Process non ascii symbols When executing mb_convert_encoding all not ascii symbols converted to '?' symbol. In that case to levenshtein function passing parameters, contain only '?' symbols.
    if use function utf8_to_extended_ascii from the comment in https://www.php.net/manual/en/function.levenshtein.php page, all non ascii symbols processed normally

--- tests/LevenshteinTest.php
+++ tests/LevenshteinTest.php
@@ -12,7 +12,12 @@ class LevenshteinTest extends TestCase
         $this->assertSame(1, Levenshtein::distance('hello', 'helo'));
         $this->assertSame(2, Levenshtein::distance('hello', 'heo'));
         $this->assertSame(1, Levenshtein::distance('héllo', 'hello'));
+        $this->assertSame(2, Levenshtein::distance('Ñörbärm', 'Üörbarm'));
         $this->assertSame(2, Levenshtein::distance('garçonnière', 'garconniere'));
         $this->assertSame(1, Levenshtein::distance('garçonnière', 'garçonniere'));
+        $this->assertSame(1, Levenshtein::distance('пожар', 'пажар'));
+        $this->assertSame(1, Levenshtein::distance('пожар', 'пожаr'));
+        $this->assertSame(2, Levenshtein::distance('слово', 'слива'));
+        $this->assertNotSame(0, Levenshtein::distance('стул', 'вода'));
     }
 }
