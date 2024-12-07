24e16d7d3ded97d5efebc84a45526038ca375d26
Martin Auswöger <martin@auswoeger.com>
2024-12-07 01:25:45 +0100

    Adhere to the provided cost values

--- src/DamerauLevenshtein.php
+++ src/DamerauLevenshtein.php
@@ -28,6 +28,10 @@ class DamerauLevenshtein
      */
     public static function distance(string $string1, string $string2, int $maxDistance = PHP_INT_MAX, int $insertionCost = 1, int $replacementCost = 1, int $deletionCost = 1, int $transpositionCost = 1): int
     {
+        if ($insertionCost < 1 || $replacementCost < 1 || $deletionCost < 1 || $transpositionCost < 1) {
+            throw new \InvalidArgumentException('Cost values below 1 are not supported');
+        }
+
         // Strip common prefix
         $xorLeft = $string1 ^ $string2;
         if ($commonPrefixLength = strspn($xorLeft, "\0")) {
@@ -52,25 +56,33 @@ class DamerauLevenshtein
         $string2Length = \count($chars2);
 
         if ($string1Length === 0) {
-            return min($maxDistance, $string2Length);
+            return min($maxDistance, $string2Length * $insertionCost);
         }
 
         if ($string2Length === 0) {
-            return min($maxDistance, $string1Length);
+            return min($maxDistance, $string1Length * $deletionCost);
         }
 
-        $maxLength = max($string1Length, $string2Length);
-        $maxDistance = min($maxDistance, $maxLength);
+        // Distance can never be higher than deleting string1 and inserting string2
+        $maxDistance = min($maxDistance, $string1Length * $deletionCost + $string2Length * $insertionCost);
 
-        $maxDeletions = floor(($maxDistance - ($string1Length - $string2Length)) / 2);
-        $maxInsertions = floor(($maxDistance + ($string1Length - $string2Length)) / 2);
+        $requiredDeletions = max(0, $string1Length - $string2Length);
+        $requiredInsertions = max(0, $string2Length - $string1Length);
 
-        $matrixSize = 1 + $maxDeletions + $maxInsertions;
+        // Distance required to bring both strings to the same length
+        $lengthDistance = $requiredInsertions * $insertionCost + $requiredDeletions * $deletionCost;
 
         // Length difference is too big
-        if ($matrixSize <= 1 || $maxDistance <= abs($string1Length - $string2Length)) {
+        if ($maxDistance <= $lengthDistance) {
             return $maxDistance;
         }
+
+        // After length correction, how many deletion/insertion pairs are maximally possible
+        $maxDeletionInsertionPairs = max(0, floor(($maxDistance - $lengthDistance) / ($deletionCost + $insertionCost)));
+
+        $maxDeletions = $requiredDeletions + $maxDeletionInsertionPairs;
+        $maxInsertions = $requiredInsertions + $maxDeletionInsertionPairs;
+        $matrixSize = 1 + $maxDeletions + $maxInsertions;
 
         // We only store the latest two rows and flip the access between them.
         $matrix = [
@@ -78,22 +90,30 @@ class DamerauLevenshtein
             array_fill(0, $matrixSize, $maxDistance),
         ];
 
-        for ($i = $maxInsertions; $i < $matrixSize; ++$i) {
-            $matrix[0][$i] = $i - $maxInsertions;
+        // Fill the row before the first one starting with 0
+        for ($i = $maxDeletions; $i < $matrixSize; ++$i) {
+            $matrix[0][$i] = ($i - $maxDeletions) * $insertionCost;
         }
 
+        // Iterate through string1 (rows)
         for ($i = 0; $i < $string1Length; ++$i) {
             $currentRow = ($i + 1) % 2;
             $lastRow = $i % 2;
+
+            // Iterate through string2 (columns)
             for ($j = 0; $j < $matrixSize; ++$j) {
-                $col = $j - $maxInsertions + $i;
+                $col = $j - $maxDeletions + $i;
+
+                // Fill the column before the first one starting with 0
                 if ($col < 0) {
-                    $matrix[$currentRow][$j] = $i - $col;
+                    $matrix[$currentRow][$j] = ($i - $col) * $deletionCost;
                     continue;
                 }
+
                 if ($col >= $string2Length) {
                     continue;
                 }
+
                 if ($i && ($chars1[$i] ?? '') === ($chars2[$col - 1] ?? '') && ($chars1[$i - 1] ?? '') === ($chars2[$col] ?? '')) {
                     // In this case $matrix[$currentRow][$j] refers to the value
                     // two rows above and two columns to the left in the matrix.
@@ -101,6 +121,7 @@ class DamerauLevenshtein
                 } else {
                     $transpositioned = $maxDistance;
                 }
+
                 $matrix[$currentRow][$j] = min(
                     $transpositioned,
                     ($matrix[$lastRow][$j + 1] ?? $maxDistance) + $deletionCost,
@@ -109,11 +130,12 @@ class DamerauLevenshtein
                 );
             }
 
-            if (min($matrix[$currentRow]) >= $maxDistance) {
+            if (min($matrix[$currentRow]) >= $maxDistance && min($matrix[$lastRow]) + $transpositionCost >= $maxDistance) {
                 return $maxDistance;
             }
         }
 
-        return min($maxDistance, $matrix[$currentRow ?? 0][$maxInsertions - ($string1Length - $string2Length)]);
+        // Return the distance value found in the last row in the last column
+        return min($maxDistance, $matrix[$currentRow ?? 0][$maxDeletions - ($string1Length - $string2Length)]);
     }
 }
